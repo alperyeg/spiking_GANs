ea44c58001710f95f55bbbcc780b7e47b75c543c
Alper Yegenoglu <a.yegenoglu@fz-juelich.de>
2017-09-26 13:59:54 +0200

    changed code to accept binned spike trains

--- src/dcgan_spikes_torch.py
+++ src/dcgan_spikes_torch.py
@@ -2,6 +2,8 @@ from __future__ import print_function
 import argparse
 import os
 import random
+import numpy as np
+import quantities as pq
 import torch
 import torch.nn as nn
 import torch.nn.parallel
@@ -11,7 +13,9 @@ import torch.utils.data
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
+from torch.utils.data import TensorDataset
 from torch.autograd import Variable
+from data_generation import DataDistribution
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', required=False,
@@ -53,7 +57,7 @@ except OSError:
     pass
 
 if opt.manualSeed is None:
-    opt.manualSeed = random.randint(1, 10000)
+    opt.manualSeed = 123
 print("Random Seed: ", opt.manualSeed)
 random.seed(opt.manualSeed)
 torch.manual_seed(opt.manualSeed)
@@ -63,7 +67,8 @@ if opt.cuda:
 cudnn.benchmark = True
 
 if torch.cuda.is_available() and not opt.cuda:
-    print("WARNING: You have a CUDA device, so you should probably run with --cuda")
+    print(
+        "WARNING: You have a CUDA device, so you should probably run with --cuda")
 
 if opt.dataset in ['imagenet', 'folder', 'lfw']:
     # folder dataset
@@ -72,7 +77,8 @@ if opt.dataset in ['imagenet', 'folder', 'lfw']:
                                    transforms.Scale(opt.imageSize),
                                    transforms.CenterCrop(opt.imageSize),
                                    transforms.ToTensor(),
-                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                                   transforms.Normalize((0.5, 0.5, 0.5),
+                                                        (0.5, 0.5, 0.5)),
                                ]))
 elif opt.dataset == 'lsun':
     dataset = dset.LSUN(db_path=opt.dataroot, classes=['bedroom_train'],
@@ -80,27 +86,51 @@ elif opt.dataset == 'lsun':
                             transforms.Scale(opt.imageSize),
                             transforms.CenterCrop(opt.imageSize),
                             transforms.ToTensor(),
-                            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                            transforms.Normalize((0.5, 0.5, 0.5),
+                                                 (0.5, 0.5, 0.5)),
                         ]))
 elif opt.dataset == 'cifar10':
+    print('in cifar10 dataset')
     dataset = dset.CIFAR10(root=opt.dataroot, download=True,
                            transform=transforms.Compose([
                                transforms.Scale(opt.imageSize),
                                transforms.ToTensor(),
                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                            ]))
+    nc = 3
 elif opt.dataset == 'fake':
     dataset = dset.FakeData(image_size=(3, opt.imageSize, opt.imageSize),
                             transform=transforms.ToTensor())
+
+# Create dataset
+else:
+    binned = []
+    num_samples = 10
+    # TODO make the range parameter variable
+    for _ in range(num_samples):
+        data = DataDistribution.poisson_nonstat_sample(t_stop=10000 * pq.ms,
+                                                       num_bins=64,
+                                                       num_sts=64)[0].to_array().ravel()
+        data = data.reshape((1, 64, 64))
+        # TODO how to normalize binned data
+        data /= np.max(data)
+        data = (data - data.mean()) / data.std()
+        binned.append(data)
+    tensor_all = torch.from_numpy(np.array(binned, dtype=np.float32))
+    # preprocess(tensor_all)
+    targets = torch.ones(num_samples)
+    dataset = TensorDataset(tensor_all, targets)
+    nc = 1
+
 assert dataset
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batchSize,
-                                         shuffle=True, num_workers=int(opt.workers))
+                                         shuffle=True,
+                                         num_workers=int(opt.workers))
 
 ngpu = int(opt.ngpu)
 nz = int(opt.nz)
 ngf = int(opt.ngf)
 ndf = int(opt.ndf)
-nc = 3
 
 
 # custom weights initialization called on netG and netD
@@ -119,7 +149,7 @@ class _netG(nn.Module):
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d(     nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
             nn.BatchNorm2d(ngf * 8),
             nn.ReLU(True),
             # state size. (ngf*8) x 4 x 4
@@ -131,21 +161,21 @@ class _netG(nn.Module):
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
             # state size. (ngf*2) x 16 x 16
-            nn.ConvTranspose2d(ngf * 2,     ngf, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
             nn.BatchNorm2d(ngf),
             nn.ReLU(True),
             # state size. (ngf) x 32 x 32
-            nn.ConvTranspose2d(    ngf,      nc, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
             nn.Tanh()
             # state size. (nc) x 64 x 64
         )
 
-    def forward(self, input):
-        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
-            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+    def forward(self, inpt):
+        if isinstance(inpt.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            out = nn.parallel.data_parallel(self.main, inpt, range(self.ngpu))
         else:
-            output = self.main(input)
-        return output
+            out = self.main(inpt)
+        return out
 
 
 netG = _netG(ngpu)
@@ -155,9 +185,9 @@ if opt.netG != '':
 print(netG)
 
 
-class _netD(nn.Module):
+class _net_D(nn.Module):
     def __init__(self, ngpu):
-        super(_netD, self).__init__()
+        super(_net_D, self).__init__()
         self.ngpu = ngpu
         self.main = nn.Sequential(
             # input is (nc) x 64 x 64
@@ -180,16 +210,16 @@ class _netD(nn.Module):
             nn.Sigmoid()
         )
 
-    def forward(self, input):
-        if isinstance(input.data, torch.cuda.FloatTensor) and self.ngpu > 1:
-            output = nn.parallel.data_parallel(self.main, input, range(self.ngpu))
+    def forward(self, inpt):
+        if isinstance(inpt.data, torch.cuda.FloatTensor) and self.ngpu > 1:
+            out = nn.parallel.data_parallel(self.main, inpt, range(self.ngpu))
         else:
-            output = self.main(input)
+            out = self.main(inpt)
 
-        return output.view(-1, 1).squeeze(1)
+        return out.view(-1, 1).squeeze(1)
 
 
-netD = _netD(ngpu)
+netD = _net_D(ngpu)
 netD.apply(weights_init)
 if opt.netD != '':
     netD.load_state_dict(torch.load(opt.netD))
@@ -197,7 +227,7 @@ print(netD)
 
 criterion = nn.BCELoss()
 
-input = torch.FloatTensor(opt.batchSize, 3, opt.imageSize, opt.imageSize)
+input_ = torch.FloatTensor(opt.batchSize, 1, opt.imageSize, opt.imageSize)
 noise = torch.FloatTensor(opt.batchSize, nz, 1, 1)
 fixed_noise = torch.FloatTensor(opt.batchSize, nz, 1, 1).normal_(0, 1)
 label = torch.FloatTensor(opt.batchSize)
@@ -208,7 +238,7 @@ if opt.cuda:
     netD.cuda()
     netG.cuda()
     criterion.cuda()
-    input, label = input.cuda(), label.cuda()
+    input_, label = input_.cuda(), label.cuda()
     noise, fixed_noise = noise.cuda(), fixed_noise.cuda()
 
 fixed_noise = Variable(fixed_noise)
@@ -228,9 +258,9 @@ for epoch in range(opt.niter):
         batch_size = real_cpu.size(0)
         if opt.cuda:
             real_cpu = real_cpu.cuda()
-        input.resize_as_(real_cpu).copy_(real_cpu)
+        input_.resize_as_(real_cpu).copy_(real_cpu)
         label.resize_(batch_size).fill_(real_label)
-        inputv = Variable(input)
+        inputv = Variable(input_)
         labelv = Variable(label)
 
         output = netD(inputv)
@@ -254,24 +284,27 @@ for epoch in range(opt.niter):
         # (2) Update G network: maximize log(D(G(z)))
         ###########################
         netG.zero_grad()
-        labelv = Variable(label.fill_(real_label))  # fake labels are real for generator cost
+        labelv = Variable(
+            label.fill_(real_label))  # fake labels are real for generator cost
         output = netD(fake)
         errG = criterion(output, labelv)
         errG.backward()
         D_G_z2 = output.data.mean()
         optimizerG.step()
 
-        print('[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
-              % (epoch, opt.niter, i, len(dataloader),
-                 errD.data[0], errG.data[0], D_x, D_G_z1, D_G_z2))
+        print(
+            '[%d/%d][%d/%d] Loss_D: %.4f Loss_G: %.4f D(x): %.4f D(G(z)): %.4f / %.4f'
+            % (epoch, opt.niter, i, len(dataloader),
+               errD.data[0], errG.data[0], D_x, D_G_z1, D_G_z2))
         if i % 100 == 0:
             vutils.save_image(real_cpu,
-                    '%s/real_samples.png' % opt.outf,
-                    normalize=True)
+                              '%s/real_samples.png' % opt.outf,
+                              normalize=True)
             fake = netG(fixed_noise)
             vutils.save_image(fake.data,
-                    '%s/fake_samples_epoch_%03d.png' % (opt.outf, epoch),
-                    normalize=True)
+                              '%s/fake_samples_epoch_%03d.png' % (
+                                  opt.outf, epoch),
+                              normalize=True)
 
     # do checkpointing
     torch.save(netG.state_dict(), '%s/netG_epoch_%d.pth' % (opt.outf, epoch))
