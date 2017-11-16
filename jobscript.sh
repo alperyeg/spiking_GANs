1ce93061a3d916ca9181439e891f4df2f2ceca9b
Alper Yegenoglu <a.yegenoglu@fz-juelich.de>
2017-11-16 15:10:01 +0100

    modified jobsript and save_data_mpi

--- jobscript.sh
+++ jobscript.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name spiking_gans
+#SBATCH --job-name genesis
 #SBATCH --workdir /home/a.yegenoglu/projects/spiking_GANs/src/
 #SBATCH -o /home/a.yegenoglu/qsub/sgans/sgans.%A.%a.out
 #SBATCH -e /home/a.yegenoglu/qsub/sgans/sgans.%A.%a.err
 # SBATCH --time 20:00:00
-# SBATCH --ntasks-per-node=10
+#SBATCH -N 5
+#SBATCH --ntasks-per-node=48
+#SBATCH --cpus-per-task=1
 # SBATCH --ntasks=24
 #SBATCH --mail-type=END
-#SBATCH --exclude=blaustein01,blaustein22
+# SBATCH --exclude=blaustein01,blaustein22
 source activate py3
 module purge
 # module load pystuff_new
-python dcgan_spikes_torch.py
+module load slurm/default
+mpirun python /home/a.yegenoglu/projects/spiking_GANs/src/save_data_mpi.py
--- src/save_data_mpi.py
+++ src/save_data_mpi.py
@@ -12,10 +12,9 @@ size = comm.Get_size()
 '''
 Supported data types are so far (step_rate | variability)
 '''
-data_type = 'step_rate'
-
+data_type = 'variability'
 # Number of data samples
-num_samples = size - 1
+num_samples = 10000
 imageSize = 64
 save_dict = {}
 # print('generating data')
@@ -29,22 +28,27 @@ recv_data = np.empty((num_samples, 1, imageSize, imageSize),
                      dtype=np.float32)
 # raw_data = []
 if rank != 0:
-    if data_type == 'step_rate':
-        data, spikes, _ = DataDistribution.poisson_nonstat_sample(
-            t_stop=10000 * pq.ms,
-            num_bins=64,
-            num_sts=64)
-    elif data_type == 'variability':
-        data, spikes = DataDistribution.gen_nonstat_sample(6, t=10000 * pq.ms,
-                                                           sample_period=10 * pq.ms,
-                                                           num_bins=64,
-                                                           num_sts=64)
-    data = data.to_array().ravel()
-    # raw_data.append(spikes)
-    # Reshape to required format
-    data = data.reshape((1, imageSize, imageSize))
-    recv_data[rank - 1] = data
-    comm.Send(recv_data, dest=0)
+    for i in range((rank - 1) * 42, (rank) * 42): 
+        if i == 10000:
+            break
+        if i % 10 == 0:
+            print(rank, i)
+        if data_type == 'step_rate':
+            data, spikes, _ = DataDistribution.poisson_nonstat_sample(
+                t_stop=10000 * pq.ms,
+                num_bins=64,
+                num_sts=64)
+        elif data_type == 'variability':
+            data, spikes = DataDistribution.gen_nonstat_sample(6, t=10000 * pq.ms,
+                                                               sample_period=10 * pq.ms,
+                                                               num_bins=64,
+                                                               num_sts=64)
+            data = data.to_array().ravel()
+            # raw_data.append(spikes)
+            # Reshape to required format
+            data = data.reshape((1, imageSize, imageSize))
+            recv_data[rank - 1] = data
+            comm.Send(recv_data, dest=0)
 # print('done generating data, in sec: {}'.format(time.time() - t))
 
 if rank == 0:
@@ -68,10 +72,3 @@ if comm.rank == 0:
     save_dict['data_type'] = data_type
     utils.save_samples(save_dict, path='.', filename='data_NS{}_IS{}_type-{}.npy'.format(num_samples,
                                                                                          imageSize, data_type))
-
-
-        
-        
-        
-        
-
